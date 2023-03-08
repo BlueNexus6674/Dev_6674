eb8a800a40ae1b8590e5f9629dbd437b3ac4dcf4
BlueNexus6674 <lwalker6674@gmail.com>
2023-03-08 20:54:32 +0000

    3

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 import os
 from setuptools import setup
 
+import pip
+import subprocess
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'my_package'])
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
